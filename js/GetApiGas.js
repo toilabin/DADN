df494b079a3e74c455be4e7f2e1077d2e8e891d4
quyetnguyen134 <quyet.nguyen134@hcmut.edu.vn>
2022-04-07 23:17:33 +0700

    update table

---
+++ js/GetApiGas.js
@@ -0,0 +1,14 @@
+$.getJSON("https://io.adafruit.com/api/v2/toilabin/feeds/bbc-gas/data?X-AIO-Key=aio_iPXE967AnIwtpOBRdiNC6qWguHFp",function(data){
+    var table = document.getElementById('infoTableGas');
+
+    var dataLength = data.length;
+
+    for (var i = 0; i < dataLength; i++) {
+        var row = table.insertRow();
+        var cell = row.insertCell();
+        cell.innerHTML = data[i].created_at;
+        var cell2 = row.insertCell();
+        cell2.innerHTML = data[i].value;
+    }
+    console.log(data)
+})
---
+++ js/GetApiSucoGas.js
@@ -0,0 +1,18 @@
+$.getJSON("https://io.adafruit.com/api/v2/toilabin/feeds/bbc-temp/data?X-AIO-Key=aio_iPXE967AnIwtpOBRdiNC6qWguHFp",function(data){
+    var table = document.getElementById('incidentTableGas');
+
+    var dataLength = data.length;
+
+    for (var i = 0; i < dataLength; i++) {
+        if (data[i].value > 100) {
+            var row = table.insertRow();
+            var cell = row.insertCell();
+            cell.innerHTML = data[i].created_at;
+            var cell2 = row.insertCell();
+            cell2.innerHTML = data[i].value;
+            var cell3 = row.insertCell();
+            cell3.innerHTML = data[i].id;
+        }
+    }
+    console.log(data)
+})
